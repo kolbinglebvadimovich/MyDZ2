837702c20c9a937c7abd078bc0e910f91763c5e2
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-02-10 21:22:00 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 int main()
 
 {
-    int variable{ 2000 };
-    std::cout << "Variable: " << variable << std::endl;
-
+    int variable1{ 2000 }; //Создание и инциализация переменной
+    std::cout << "Variable :" << std::endl;
+    variable2 = 3000;
+    std::cout << "Variable :" << std::endl;
+    
     return EXIT_SUCCESS;
 }
+
 
 
 
