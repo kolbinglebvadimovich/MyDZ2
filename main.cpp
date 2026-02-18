3954c5c80532e9b0a4befba04f414cc77996a0e6
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-02-18 20:14:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,14 @@ int main()
 
 {
     int variable1{ 2000 }; //Создание и инциализация переменной
-    std::cout << "Variable :" << std::endl;
     variable2 = 3000;
+    std::cout << "Введите число" << std::endl;
+    std::cin >> "Variable :" >> std::endl;
     std::cout << "Variable :" << std::endl;
-    {
-        double variable { -555.9 };
-        std::cout << variable << std::endl;
-    }
     
     return EXIT_SUCCESS;
 }
+
 
 
 
