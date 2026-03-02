1180c335dc088a0cc53402724dcc651e43dd13ed
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-03-02 20:00:27 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 
 int main()
-
 {
-    int variable { 2000 }; 
+    int variable { 2000 };
+
     std::cout << "Введите число" << std::endl;
-    std::cin >> variable >> std::endl;
+    std::cin >> variable;
     std::cout << variable << std::endl;
-    
-    return EXIT_SUCCESS;
+
+    return 0;
 }
-
-
-
-
-
-
-
-
-
